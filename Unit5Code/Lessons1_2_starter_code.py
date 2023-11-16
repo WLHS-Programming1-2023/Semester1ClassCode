de123b87189405ef42f343d539fb9fccafa1f16b
smithbwlhs <smithb@wlwv.k12.or.us>
2023-11-16 10:39:16 -0800

    Create Lessons1_2_starter_code.py

---
+++ Unit5Code/Lessons1_2_starter_code.py
@@ -0,0 +1,46 @@
+# f strings in Python
+
+
+
+# String functions (upper, lower, strip, capitalize, isdigit, islower,isupper, in )
+
+
+# Accessing single characters within a string
+###  M   i   L   L   i   e  ####
+###  0   1   2   3   4   5  ####
+### -6  -5  -4  -3  -2  -1 #### 
+
+
+# print(cat_name[100000]) #IndexError, also known as out of range error
+
+# Number of occurences of a character in a string
+
+
+# Accessing multiple characters in a string(slicing)
+# syntax possiblities:
+# string[start:stop], string[start:], string[:stop]
+# string[:], string[start:stop:step], string[::step]
+# up to but not including the last index
+
+
+### T  h  i  s   i  s   a   s  t  r  i  n  g ###
+### 0  1  2  3 4 5  6 7 8 9 10 11 12 13 14 15
+
+# string[start:stop], string[start:], string[:stop]
+
+
+
+
+
+### T  h  i  s   i  s   a   s  t  r  i  n  g ###
+### 0  1  2  3 4 5  6 7 8 9 10 11 12 13 14 15
+
+# string[:], string[start:stop:step], string[::step]
+
+
+#negative indices
+
+
+#reverse string
+
+
