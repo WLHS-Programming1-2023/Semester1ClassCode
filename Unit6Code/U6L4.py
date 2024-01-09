83f96c2848bc6ea3a76eabc4f5448412486b5712
smithbwlhs <smithb@wlwv.k12.or.us>
2024-01-09 13:28:56 -0800

    Update U6L4.py

--- Unit6Code/U6L4.py
+++ Unit6Code/U6L4.py
@@ -2,66 +2,66 @@
 
 import random
 
-# count - returns number of times something appears
+# count - returns the number of occurences
+# of some character
+
 my_list = [1,2,3,1,2,'a','b']
 num_twos = my_list.count(2)
-count_a = my_list.count('a')
+num_as = my_list.count('a')
 #print(num_twos)
-#print(count_a)
+#print(num_as)
 
 # append
 my_list.append('c')
 #print(my_list)
-#print(my_list.append('d')) # will not print list, prints None
+last_item = my_list.pop()
+#print(last_item)
 #print(my_list)
-
-# pop - removes and returns last item from a list 
+my_list.append(['c','d'])
+#print(my_list)
 my_list.pop()
-#print(my_list)
-for i in range(3):
-    my_list.pop()
-#print(my_list)
-
-# more appending
-my_list.append(['a','b','c'])
+# extend - adds the items in a 
+# list/tuple to the end of a list
+my_list.extend('c')
 #print(my_list)
 
-#extend - adds list of items to the end of a list
-my_list.pop()
-my_list.extend(['a','b','c'])
-#print(my_list)
-
-# .sort() vs sorted(list)
+# .sort() vs sorted()
 num_list = []
 for i in range(20):
     num_list.append(random.randint(1,100))
-
-print(num_list)
-sorted_list = num_list.sort()
-print(sorted_list)
-print(num_list)
-# issue with .sort() is that it permanently
-# changes the list in memory - cannot go back
-# because lists are mutable
-print(""*5)
-num_list2 = []
-for i in range(20):
-    num_list2.append(random.randint(1,100))
-sorted_list2 = sorted(num_list2)
-print(num_list2)
-print(sorted_list2)
-
-print("\n"*3)
-my_chars = ['a','A','b','$','{','(']
-# [97,65,98,36,123,40] are the ASCII values
-# and it sorts their ASCII values then
-# converts back
-print(sorted(my_chars))
+#print(num_list)
+#print(num_list.sort())
+#print(num_list) # original num_list no longer exists
+                # since lists are mutable
+#sorted_list = num_list.sort()
+#print(sorted_list)
+# sorted_list = sorted(num_list)
+# sorted_list.reverse()
+# print(num_list)
+# print(sorted_list)
+my_animals= ['Aardvark','Camel','Bear','Rhino']
+# ascii values are [97,65,98,36,123,40]
+#print(sorted(my_animals))
 
 my_nums = [1,2,3,4,5]
-my_nums.reverse()
-print(my_nums)
-my_nums.remove(5)
-print(my_nums)
+my_nums.pop()
+#print(my_nums)
+val = my_nums.pop(1)
+#print(my_nums)
+#print(val)
+
+final_list = []
+for i in range(20):
+    final_list.append(random.randint(1,100))
+
+# remove 20
+
+try:   
+    location = final_list.index(20)
+    final_list.pop(location)
+    print(f"Item removed from index {location}")
+except ValueError:
+    print("Number not found")
+
 
 
